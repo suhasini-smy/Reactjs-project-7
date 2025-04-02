a31019aa25d95e850689cf874b9eb23867d69253
s s <75931684+suhasini-smy@users.noreply.github.com>
2025-04-02 17:22:26 +0530

    project8

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import { useRef, useState } from "react";
 
 const App = () => {
+  const [count, setCount] = useState(0);
+  const ref = useRef(0);
+
+  const handleSubmit = () => {
+    setCount(count + 1);
+
+    ref.current++;
+
+    console.log("State Count", count);
+    console.log("Ref Count", ref.current);
+  };
+
   return (
     <>
-      <div>App</div>
+      <div>Count:{count}</div>
+      <button onClick={handleSubmit}>Increment</button>
     </>
   );
 };
